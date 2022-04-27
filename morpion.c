0acdddaee1c758644cc1182c223a164c6f3766eb
Luvey <82052250+Luvey35@users.noreply.github.com>
2022-04-27 11:40:01 +0200

    Add rules..

--- morpion.c
+++ morpion.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <time.h>
 
 const char YES[5] = "Yes";
 const char NO[5] = "No";
@@ -11,7 +12,7 @@ typedef char players[2];
 
 // Procedures and functions
 bool menu();
-void rules();
+void rules(table);
 void initialize_board(table);
 void game(table);
 void display_board();
@@ -29,7 +30,7 @@ int main() {
 
     printf("\n");
     printf("############################\n");
-    printf("#####   GAME MORPION   #####\n");
+    printf("#####   TIC TAC TOE    #####\n");
     printf("############################\n");
     printf("\n");
 
@@ -37,7 +38,7 @@ int main() {
     printf("> ");
     scanf("%s", rules_choice);
     if (strcmp(rules_choice, YES) == 0) {
-        rules();
+        rules(board_game);
     }
 
     bool will = menu();
@@ -50,17 +51,22 @@ int main() {
             char player1[50];
             char player2[50];
 
+            printf("\n");
             printf("Enter the name of the player 1 :\n");
             printf("> ");
             scanf("%s", player1);
 
+            printf("\n");
             printf("This player will have the sign O.\n");
+            printf("\n");
 
             printf("Enter the name of the player 2 :\n");
             printf("> ");
             scanf("%s", player2);
 
+            printf("\n");
             printf("This player will have the sign X.\n");
+            printf("\n");
 
             //table[0] = player1;
             //table[1] = player2;
@@ -100,6 +106,7 @@ int main() {
 
 // Menu procedure
 bool menu() {
+
     bool answer;
 
     int choice;
@@ -131,8 +138,57 @@ bool menu() {
 }
 
 // Rules procedure
-void rules() {
-    printf("rules...\n");
+void rules(table t) {
+
+    printf("\n");
+    printf("Here are the rules :\n");
+    sleep(3);
+    printf("\n");
+    printf("The Tic Tac Toe game is played between 2 players");
+    sleep(4);
+    printf(", each player has a sign, one has the X sign and the other the O sign.\n");
+    sleep(4);
+    printf("This takes place on a board of 9 squares that the players can fill with their signs.\n");
+    sleep(4);
+    printf("At each turn, a player chooses a square and to win you have to line up in 3. \n");
+    sleep(4);
+    printf("However, a player cannot put his sign on a square already filled.\n");
+    sleep(4);
+    printf("\n");
+    printf("The board looks like this :\n");
+    sleep(1);
+    printf("\n");
+    display_board(t);
+    sleep(4);
+    printf("\n");
+    printf("The numbers 1, 2 and 3 on the left of the board indicate the row number");
+    sleep(4);
+    printf(", and those above, the column number.\n");
+    sleep(4);
+    printf("For a player to play, he must select the row and column number to place his sign.\n");
+    sleep(4);
+    printf("For example, if the player wants to place his sign in the middle of the board");
+    sleep(4);
+    t[1][1] = 'X';
+    printf(", he has to choose the row 2 and the column 2, and here is the result : \n");
+    sleep(3);
+    printf("\n");
+    display_board(t);
+    sleep(3);
+    printf("\n");
+    printf("If the player wants to place in the bottom right of the board, the row 3 and the column 3, and here is the result :\n");
+    t[1][1] = '.';
+    t[2][2] = 'X';
+    sleep(4);
+    printf("\n");
+    display_board(t);
+    printf("\n");
+    sleep(4);
+    printf("And as long as the board is not full or neither of the 2 players has succeeded in aligning 3 of them, the game continues.\n");
+    sleep(5);
+    printf("Good game :)\n");
+    sleep(2);
+
 }
 
 void initialize_board(table t) {
